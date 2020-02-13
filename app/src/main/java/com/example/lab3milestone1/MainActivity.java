24ed014bd8e39810f87e82e7edc721c26b3270fa
Mak <mak.yuwen@gmail.com>
2020-02-13 13:24:12 -0600

    Added full functionality, including menu buttons, dialog box, and submenu!

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.lab3milestone1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button =  findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.ex_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.first:
+                Toast.makeText(this, ":)", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.second:
+                Toast.makeText(this, "YEEEEEEEEET", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.sub1:
+                Toast.makeText(this, "Lmaooooo", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.sub2:
+                Toast.makeText(this,"Loser", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openDialog(){
+        DialogThing dialogBox = new DialogThing();
+        dialogBox.show(getSupportFragmentManager(), "Example Dialog");
     }
 }
